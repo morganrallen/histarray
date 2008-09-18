94e61b75b6bd84b7f56605ac18b5a4a9b8ac45a4
morgan <morgan@dirtdirty.zossener53.olsr>
2008-09-18 18:50:27 +0200

    initial histarray. support creating new histarrays, pushing new revisions, moving the timeline to different revisions, and editing anywhere on the timeline.

---
+++ histarray.js
@@ -0,0 +1,93 @@
+function histarray()
+{
+	for(var i = 0; i < arguments.length; i++)
+		this._add(arguments[i], i);
+
+	this.__defineSetter__('revision', function(v)
+	{
+		return (v > this.length) ?
+			this.revision :
+			this._revision = v;
+	});
+	this.__defineGetter__('revision', function()
+	{
+		return this._revision;
+	});
+	this.__defineGetter__('prev', function()
+	{
+		return (this.revision != 0) ?
+			(function(self)
+			{
+				var a = Array();
+				for(var i in self._elements)
+					a.push(self._elements[i][self.revision - 1]);
+				return a;
+			})(this) :
+			null;
+	});
+	this.__defineGetter__('nxt', function()
+	{
+		return (this.revision < this.revisions) ?
+			(function(self)
+			{
+				var a = Array();
+				for(var i in self._elements)
+					a.push(self._elements[i][self.revision + 1]);
+				return a;
+			})(this) :
+			null;
+	});
+};
+
+histarray.prototype =
+{
+	length: 0,
+	revisions: 0,
+	_elements: Array(),
+	_revision: 0,
+	_add: function(e, i)
+	{
+		this._elements[i] = (function(e,self)
+		{
+			var a = Array();
+			for(var i = 0; i < self.revisions; i++)
+				a.push(null);
+			a.push(e);
+			return a;
+		})(e,this);
+		this.__defineGetter__(i, function()
+		{
+			return this._elements[i][this.revision];
+		});
+
+		this.__defineSetter__(i, function(v)
+		{
+			return this._elements[i][this.revision] = v;
+		});
+		this.length = i + 1;
+	},
+	// push csv or a single array
+	push: function()
+	{
+		if(typeof arguments[0] == 'object')
+			this.push.apply(this, arguments[0]);
+		else
+		{
+			if(this.revision == this.revisions)
+			{
+				this.revision++;
+				this.revisions++;
+			}
+			for(var i = 0; i < arguments.length; i++)
+			{
+				if(arguments[i] == null) continue;
+
+				if(this._elements[i])
+					this._elements[i][this.revision] = arguments[i];
+				else
+					this._add(arguments[i], i);
+			}
+		}
+		return this.length;
+	}
+};
